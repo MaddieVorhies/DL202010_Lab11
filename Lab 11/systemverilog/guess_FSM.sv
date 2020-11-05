81429514069092e7638d5ac4dcb3603e798bff23
MaddieVorhies <maddie_vorhies1@baylor.edu>
2020-11-05 13:31:28 -0600

    Lab 11

--- Lab 11/systemverilog/guess_FSM.sv
+++ Lab 11/systemverilog/guess_FSM.sv
@@ -21,17 +21,37 @@
 
 
 module guess_FSM #(parameter N=1)(
-    input clk, reset,
-    input  b0, b1, b2, b3,
+    input clk, rst, en,
+    input  [3:0] b,
     output reg win, lose,
-    output reg y0, y1, y2, y3
+    output reg [3:0] y
     );
     
     localparam [1:0]
-      s0   = 2'b00,
-      s1  = 2'b01,
+      s0    = 2'b00,
+      s1    = 2'b01,
       s2    = 2'b11,
-      s3  = 2'b10;
-      swin = 
+      s3    = 2'b10,
+
+      
+    reg [1:0] state, state_next;
+      
+    always @(posedge clk, posedge rst)
+    begin
+       if (rst==1) 
+          win <= 0;
+          lose <= 0;
+          y[3:0] <= 0;
+       else if (en==1)
+          Q <= D;
+    end
+    
+    
+    
+    
+      
+    
+      
+    
     
 endmodule
