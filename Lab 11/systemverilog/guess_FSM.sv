4d18c9ef46006b8f8d5b563a0b6837f8fd2034c4
MaddieVorhies <maddie_vorhies1@baylor.edu>
2020-11-11 21:36:33 -0600

    Lab11

--- Lab 11/systemverilog/guess_FSM.sv
+++ Lab 11/systemverilog/guess_FSM.sv
@@ -28,29 +28,101 @@ module guess_FSM #(parameter N=1)(
     );
     
     localparam [1:0]
-      s0    = 2'b00,
-      s1    = 2'b01,
-      s2    = 2'b11,
-      s3    = 2'b10,
+      s0    = 2'b0000,
+      s1    = 2'b0001,
+      s2    = 2'b0011,
+      s3    = 2'b0010,
+      swin  = 4'b0100,
+      slose = 4'b0101;
 
-      
     reg [1:0] state, state_next;
+    reg [N-1:0] counter, counter_next;
       
-    always @(posedge clk, posedge rst)
-    begin
-       if (rst==1) 
-          win <= 0;
-          lose <= 0;
-          y[3:0] <= 0;
-       else if (en==1)
-          Q <= D;
+    always_ff @(posedge clk, posedge rst)
+       if (rst) begin
+         state   <= s0;
+         counter <= {N{1'b1}};
+      end
+      else begin
+         state   <= state_next;
+         counter <= counter_next;
+      end
+      
+    always_comb begin
+      state_next   = state;
+      counter_next = counter;
+      
+      case(state)
+         s0: begin
+            y[0] = 1;
+            if (4'b0001)
+               state_next = swin;
+            else if (4'b000)
+               state_next = s1; 
+            else 
+               state_next = slose;
+         end
+         
+         s1: begin
+            y[1] = 1;
+            if (4'b0010)
+               state_next = swin;
+            else if (4'b000)
+               state_next = s2; 
+            else 
+               state_next = slose;
+         end
+         
+         s2: begin
+            y[2] = 1;
+            if (4'b0100)
+               state_next = swin;
+            else if (4'b000)
+               state_next = s3; 
+            else 
+               state_next = slose;
+         end
+         
+         s3: begin
+            y[3] = 1;
+            if (4'b1000)
+               state_next = swin;
+            else if (4'b000)
+               state_next = s0; 
+            else 
+               state_next = slose;
+         end
+         
+         swin: begin
+            win = 1;
+            if (4'b0000)
+               state_next = s0; 
+            else 
+               state_next = swin;
+         end
+         
+         slose: begin
+            lose = 1;
+            if (4'b0000)
+               state_next = s0; 
+            else 
+               state_next = swin;
+         end
+      endcase
     end
     
+    always @*
+       case(state)
+          s0: y = 4'b0001;
+          s1: y = 4'b0010;
+          s2: y = 4'b0100;
+          s3: y = 4'b1000;
+          swin: win = 1;
+          slose: lose = 1;
+       endcase
+       
     
-    
-    
-      
-    
+          
       
     
     
